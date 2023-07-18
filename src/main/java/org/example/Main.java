6b3f1ba335095601186712a97b9e32cb1e708955
JeremiePlanta <Jeremie.Planta@stahl-holding-saar.de>
2023-07-18 11:33:55 +0200

    Der RESTfulRPCAdapter wird jetzt mit dem erstellten Objekt fahrradVerwaltung verbunden, anstelle der Klasse

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
     @SuppressWarnings("java:S106")
     public static void main(String[] args) {
         FahrradRepository fahrradRepository = new FahrradRepositoryImpl();
-        FahrradVerwaltung fahrraeder = new FahrradVerwaltung(fahrradRepository);
+        FahrradVerwaltung fahrradVerwaltung = new FahrradVerwaltung(fahrradRepository);
         StammdatenService stammdatenService = new StammdatenService(fahrradRepository);
         stammdatenService.initStammdaten();
 
@@ -20,7 +20,7 @@ public class Main {
 
         jexxaMain
                 // Bind a REST adapter to expose parts of the application
-                .bind(RESTfulRPCAdapter.class).to(FahrradVerwaltung.class)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
+                .bind(RESTfulRPCAdapter.class).to(fahrradVerwaltung)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
 
                 // Run your application until Ctrl-C is pressed
