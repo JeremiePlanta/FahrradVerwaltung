6131426a6da658cb2773dbfac6366fc6e740d31f
JeremiePlanta <Jeremie.Planta@stahl-holding-saar.de>
2023-07-21 14:38:55 +0200

    Umstellen von Main auf Dependency-Injection von Jexxa, damit in Main keine fachlichen Objekte mehr erstellt werden.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,16 +12,13 @@ import java.util.Properties;
 public class Main {
     @SuppressWarnings("java:S106")
     public static void main(String[] args) {
-        FahrradRepository fahrradRepository = new FahrradRepositoryImpl(new Properties());
-        FahrradVerwaltung fahrradVerwaltung = new FahrradVerwaltung(fahrradRepository);
-        StammdatenService stammdatenService = new StammdatenService(fahrradRepository);
-        stammdatenService.initStammdaten();
 
         var jexxaMain = new JexxaMain(Main.class);
 
         jexxaMain
+                .bootstrap(StammdatenService.class).with(StammdatenService::initStammdaten)
                 // Bind a REST adapter to expose parts of the application
-                .bind(RESTfulRPCAdapter.class).to(fahrradVerwaltung)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
+                .bind(RESTfulRPCAdapter.class).to(FahrradVerwaltung.class)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
 
                 // Run your application until Ctrl-C is pressed
