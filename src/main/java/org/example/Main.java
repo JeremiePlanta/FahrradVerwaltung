5f09b5ea164a57a42cce107f55c34c128de79d19
JeremiePlanta <Jeremie.Planta@stahl-holding-saar.de>
2023-09-19 11:31:28 +0200

    VerifizierungsCodeSender Interface für das senden des DomainEvents erstellt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,11 @@ package org.example;
 import io.jexxa.core.JexxaMain;
 import io.jexxa.drivingadapter.rest.RESTfulRPCAdapter;
 import org.example.applicationservice.FahrradVerwaltung;
+import org.example.domain.VerifizierungsCodeVerschickt;
 import org.example.domainservice.StammdatenService;
+import org.example.domainservice.VerifizierungsCodeSender;
+
+import static org.example.domain.DomainEventPublisher.subscribe;
 
 
 public class Main {
@@ -14,6 +18,7 @@ public class Main {
 
         jexxaMain
                 .bootstrap(StammdatenService.class).with(StammdatenService::initStammdaten)
+                .bootstrap(VerifizierungsCodeSender.class).with(sender ->  subscribe(VerifizierungsCodeVerschickt.class, sender::send))
                 // Bind a REST adapter to expose parts of the application
                 .bind(RESTfulRPCAdapter.class).to(FahrradVerwaltung.class)               // Get greetings: http://localhost:7501/HelloJexxa/greetings
                 .bind(RESTfulRPCAdapter.class).to(jexxaMain.getBoundedContext())  // Get stats: http://localhost:7501/BoundedContext/isRunning
